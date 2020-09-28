c9d138840434ec0bd7a9c9a9f36677685b7267e7
William Barela <william.barela.dev@gmail.com>
2020-09-28 18:57:36 -0500

    add custom css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './css/index.css';
 import * as serviceWorker from './other/serviceWorker';
 
 ReactDOM.render(
