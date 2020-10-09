2e7fd10428e2ad5d6d5cd5a49803034edde51a3b
William Barela <william.barela.dev@gmail.com>
2020-10-09 17:58:24 -0500

    ensure SOC by moving Weeks to its own file

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,22 +2,9 @@ import React from 'react';
 
 import '../css/dashboard.css';
 
-import YearSelector from './dashboard/YearSelector';
 import Logo from './Logo';
-
-function Weeks() {
-  return(
-    <div className="weeks-card">
-      <ul className="weeks">
-        <li><a href="#">42</a></li>
-        <li><a href="#">43</a></li>
-        <li className="item-selected"><a href="#">44</a></li>
-        <li><a href="#">45</a></li>
-        <li><a href="#">46</a></li>
-      </ul>
-    </div>
-  )
-}
+import YearSelector from './dashboard/YearSelector';
+import Weeks from './dashboard/Weeks';
 
 function Months() {
   return(
---
+++ src/components/dashboard/Weeks.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Weeks() {
+  return(
+    <div className="weeks-card">
+      <ul className="weeks">
+        <li><a href="#">42</a></li>
+        <li><a href="#">43</a></li>
+        <li className="item-selected"><a href="#">44</a></li>
+        <li><a href="#">45</a></li>
+        <li><a href="#">46</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Weeks;
