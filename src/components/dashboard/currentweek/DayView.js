559dfabc28e8a07e75343a438d1aa345a476bfde
William Barela <william.barela.dev@gmail.com>
2020-10-11 13:21:37 -0500

    add month to day to avoid confusion when month is different in the week

--- src/components/dashboard/currentweek/DayView.js
+++ src/components/dashboard/currentweek/DayView.js
@@ -4,6 +4,7 @@ function DayView(props){
   const selectedDate = (props.day.valueOf() === props.currentDateObject.valueOf()) ? "day-selected" : "";
   const date = props.day.getDate();
   const day = props.day.toLocaleDateString(undefined, { weekday: 'short' });
+  const month = props.day.toLocaleDateString(undefined, { month: 'short' });
 
   return(
     <div className="day-view-card">
@@ -15,8 +16,11 @@ function DayView(props){
         </ul>
       </div>
       <div className={`day-card ${selectedDate}`}>
-        <span>{day}</span>
-        <a href="#">{date}</a>
+        <a href="#">
+        <span className="day">{day}</span>
+        <span className="date">{date}</span>
+        <span className="month">{month}</span>
+        </a>
       </div>
     </div>
   )
