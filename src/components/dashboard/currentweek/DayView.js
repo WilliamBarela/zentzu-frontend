efb5d43a0bc873881acc33625bb68d7a55613a75
William Barela <william.barela.dev@gmail.com>
2020-10-13 03:28:25 -0500

    remove task list

--- src/components/dashboard/currentweek/DayView.js
+++ src/components/dashboard/currentweek/DayView.js
@@ -8,13 +8,7 @@ function DayView(props){
 
   return(
     <div className="day-view-card">
-      <div className="task-summary-card">
-        <ul className="task-list">
-          <li><span className="task-urgent">3</span></li>
-          <li><span className="task-normal">1</span></li>
-          <li><span className="task-completed">5</span></li>
-        </ul>
-      </div>
+      <div className="task-summary-card"></div>
       <div className={`day-card ${selectedDate}`}>
         <a href="#">
         <span className="day">{day}</span>
