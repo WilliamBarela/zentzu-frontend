7eb31f10c08302d31da1f7e04b975e77c9711a4d
William Barela <william.barela.dev@gmail.com>
2020-10-22 23:02:23 -0500

    change base API route based on hostname of frontend

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,4 +1,4 @@
-const ROOT = "http://localhost:3001/api/v1";
+const ROOT = (window.location.hostname === "williambarela.com") ? "https://zentzu.herokuapp.com/api/v1" : "http://localhost:3001/api/v1";
 
 export const URI = (endpoint) => ROOT + endpoint;
 
