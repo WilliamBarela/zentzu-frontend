2805521c6446b7423b0c31733e8a059de90b9fd1
William Barela <william.barela.dev@gmail.com>
2020-10-22 23:22:47 -0500

    Updates

---
+++ precache-manifest.6d0515bed3605abc3de37ff157a58a16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4c3992b8c32aeb457e4f52a9ee33945",
+    "url": "/zentzu-frontend/index.html"
+  },
+  {
+    "revision": "a759d952b995187f0690",
+    "url": "/zentzu-frontend/static/css/main.08932816.chunk.css"
+  },
+  {
+    "revision": "bf77c0daaed8c7a2ffb2",
+    "url": "/zentzu-frontend/static/js/2.77c567b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/zentzu-frontend/static/js/2.77c567b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a759d952b995187f0690",
+    "url": "/zentzu-frontend/static/js/main.a9011384.chunk.js"
+  },
+  {
+    "revision": "a6be66b38fb4b6be8dcd",
+    "url": "/zentzu-frontend/static/js/runtime-main.6ebf7ca2.js"
+  },
+  {
+    "revision": "00d59b3fd1708968456b97557d21e092",
+    "url": "/zentzu-frontend/static/media/logo.00d59b3f.svg"
+  }
+]);
--- src/api/endpoints.js
+++
@@ -1,8 +0,0 @@
-const ROOT = "http://localhost:3001/api/v1";
-
-export const URI = (endpoint) => ROOT + endpoint;
-
-export const LOGIN = "/login";
-export const PROFILE = "/profile";
-export const SIGNUP = "/signup";
-export const DASHBOARD = "/dashboard";
